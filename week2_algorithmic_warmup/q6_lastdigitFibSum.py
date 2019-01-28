fe64aa85b68a8ce32e24c3d21e777a4b5bb7ddb0
andre odendaal <aodendaal.direct@gmail.com>
2019-01-27 19:14:14 -0800

    question 6, failed on time

--- week2_algorithmic_warmup/q6_lastdigitFibSum.py
+++ week2_algorithmic_warmup/q6_lastdigitFibSum.py
@@ -1,20 +1,19 @@
 # Uses python3
 import sys
-from collections import deque
 
 def q6_lastdigitFibSum(p_num):
 
     if p_num == 0:
         return 0
 
-    total = 1
-    fib_lst = deque([0, 1])
+    first = 0
+    last = 1
+    total = 0
 
-    for i in range(1, p_num):
+    for _ in range(p_num):
 
-        fib_lst.append(fib_lst[-1] + fib_lst[-2])
-        total = total + fib_lst[-1]
-        fib_lst.popleft()
+        first, last, total = last, last + first, total + last
+
     str_total = str(total)
 
     return str_total[-1]
@@ -25,3 +24,4 @@ if __name__ == '__main__':
     n = int(input)
     print(q6_lastdigitFibSum(n))
 
+# 100 = 5
