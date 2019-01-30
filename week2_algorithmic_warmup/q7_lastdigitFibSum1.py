7c04e386b6cd258f16bcd3118651b963386f83ca
Andre <Odendaal>
2019-01-30 15:26:48 -0800

    Q6 success, start Q7

--- week2_algorithmic_warmup/q7_lastdigitFibSum1.py
+++ week2_algorithmic_warmup/q7_lastdigitFibSum1.py
@@ -2,18 +2,24 @@
 import sys
 
 
-def q6_lastdigitFibSum(p_num):
+def q7_lastdigitFibSum(p_start, p_num):
 
     if p_num <= 2:
         return p_num
 
     values = [0, 1]
-    total = [0, 1]
+    total = [0]
 
 
     for i in range(59):
-        values.append((values[i] + values[i + 1]) % 10)
-        total.append((sum(values)) % 10)
+
+        values.append((values[i] + values[i + 1]))
+        if i + 2 >= p_start:
+            total.append((sum(values[p_start:]) % 10))
+
+        if i + 2 == p_num:
+            return total[-1]
+
     idx = p_num % 60
     return_total = total[idx]
     return return_total
@@ -21,8 +27,8 @@ def q6_lastdigitFibSum(p_num):
 
 if __name__ == '__main__':
     input = sys.stdin.read()
-    n = int(input)
-    print(q6_lastdigitFibSum(n))
+    a, b = map(int, input.split())
+    print(q7_lastdigitFibSum(a, b))
 
 # 100 = 5
 # sum of 100  = 927372692193078999175.
