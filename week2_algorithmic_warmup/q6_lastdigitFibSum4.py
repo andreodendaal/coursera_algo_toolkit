a0c1e7dc20c7fbb3260d00ebb190ff0893eb48d9
andre odendaal <aodendaal.direct@gmail.com>
2019-01-29 23:21:03 -0800

    Q6 still not there

--- week2_algorithmic_warmup/q6_lastdigitFibSum4.py
+++ week2_algorithmic_warmup/q6_lastdigitFibSum4.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def q6_lastdigitFibSum(p_num):
 
     if p_num == 0:
@@ -8,14 +9,12 @@ def q6_lastdigitFibSum(p_num):
 
     values = [0, 1]
 
-
-    for i in range(p_num + 1):
-
+    for i in range(59):
         values.append((values[i] + values[i + 1]))
 
-        #values[0], values[1] = values[1], values[1] + values[0]
-    str_total = str(values[-1] - 1)
-    return str_total[-1]
+    idx = p_num % 60
+    return_val = values[idx]
+    return return_val
 
 
 if __name__ == '__main__':
@@ -29,3 +28,5 @@ if __name__ == '__main__':
 # 100 th value = 573147844013817084101
 # sum of 50 =    32951280098
 # 51 th value =  32951280099
+# 239 = 0
+# 240 = 0
