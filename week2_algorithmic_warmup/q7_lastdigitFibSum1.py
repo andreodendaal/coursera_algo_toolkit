8ac837cad475fe585286521f3affa7f6fc8820f8
andre odendaal <aodendaal.direct@gmail.com>
2019-01-30 19:39:25 -0800

    Q7 correct, 3 attempts!

--- week2_algorithmic_warmup/q7_lastdigitFibSum1.py
+++ week2_algorithmic_warmup/q7_lastdigitFibSum1.py
@@ -1,7 +1,6 @@
 # Uses python3
 import sys
 
-
 def q7_lastdigitFibSum(p_start, p_num):
 
     if p_num <= 2:
@@ -10,18 +9,20 @@ def q7_lastdigitFibSum(p_start, p_num):
     values = [0, 1]
     total = [0]
 
+    m_start = p_start % 60
+    m_num = p_num % 60
 
     for i in range(59):
 
         values.append((values[i] + values[i + 1]))
-        if i + 2 >= p_start:
-            total.append((sum(values[p_start:]) % 10))
 
-        if i + 2 == p_num:
+        if i + 2 >= m_start:
+            total.append((sum(values[m_start:]) % 10))
+
+        if i + 2 == m_num:
             return total[-1]
 
-    idx = p_num % 60
-    return_total = total[idx]
+    return_total = total[-1]
     return return_total
 
 
@@ -30,11 +31,6 @@ if __name__ == '__main__':
     a, b = map(int, input.split())
     print(q7_lastdigitFibSum(a, b))
 
-# 100 = 5
-# sum of 100  = 927372692193078999175.
-# 101th value = 927372692193078999176
-# 100 th value = 573147844013817084101
-# sum of 50 =    32951280098
-# 51 th value =  32951280099
-# 239 = 0
-# 240 = 0
+# 1234 12345 = 8
+# 3 7 = 1
+# 10 10 = 5
