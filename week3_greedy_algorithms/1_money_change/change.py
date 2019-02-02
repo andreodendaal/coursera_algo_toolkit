a41c8620f9685c5ea6c0b478f8eb3a9434e707a9
andre odendaal <aodendaal.direct@gmail.com>
2019-02-01 19:23:02 -0800

    week 3 question 1 correct

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -3,7 +3,19 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    coins = [10, 5, 1]
+    running_total = m
+
+    coin_total = 0
+    denom_totalValue = 0
+
+    for coin_value in coins:
+        floor = running_total // coin_value
+        coin_total += floor
+        denom_totalValue = coin_value * floor
+        running_total -= denom_totalValue
+
+    return coin_total
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
