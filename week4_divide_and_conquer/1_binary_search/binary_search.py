3cb008c002e3d98344e195491094f20097d33293
andre odendaal <aodendaal.direct@gmail.com>
2019-02-10 13:47:16 -0800

    Binary search - pass

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,9 +1,22 @@
 # Uses python3
+
+
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+    minIndex, maxIndex = 0, len(a) - 1
     # write your code here
+    while maxIndex >= minIndex:
+        midIndex = int((minIndex + maxIndex)/2)
+
+        if a[midIndex] == x:
+            return midIndex
+        elif a[midIndex] < x:
+            minIndex = midIndex + 1
+        else:
+            maxIndex = midIndex - 1
+    return -1
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +32,7 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        #print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end=' ')
+
+# 5 1 5 8 12 13 5 8 1 23 1 11
