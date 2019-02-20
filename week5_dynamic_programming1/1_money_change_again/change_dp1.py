175299ee4fdb68fe3a833ecbbf84ec7560a5862d
andreodendaal <aodendaal.direct@gmail.com>
2019-02-19 20:01:11 -0800

    Dynamic Programming - Money Change - Pass

--- week5_dynamic_programming1/1_money_change_again/change_dp1.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp1.py
@@ -3,15 +3,34 @@ import sys
 
 def get_change(money):
     #write your code here
-    minNumCoins = []
-    for m in range(money + 1):
-        minNumCoins.append(m)
+    if money == 0:
+        return 0
+    coins = [1, 3, 4]
+    numCoins = 0
+    minNumCoins = {key: money for key in range(money + 1)}
+    for m in range(1, money + 1):
 
+        for i, coin in enumerate(coins):
+            if m >= coin:
+                test = m // coin
+                test2 = m % coin
+                if m % coin == 0:
+                    numCoins = int(m/coin)
+                else:
+                    first = minNumCoins.get(m - coin)
+                    numCoins = minNumCoins.get(m - coin) + 1
 
-    return minNumCoins
+                if numCoins < minNumCoins.get(m) and numCoins != 0:
+                    minNumCoins[m] = numCoins
+
+    final = minNumCoins.get(money)
+    return final
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
     print(get_change(m))
 
-#
+# 2 = 2
+# 34 = 9
+# 6 = 2
