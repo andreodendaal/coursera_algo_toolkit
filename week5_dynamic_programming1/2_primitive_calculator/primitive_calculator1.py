00bfa7a3dbaa697c7b0377051d171ca41dd361b8
andreodendaal <aodendaal.direct@gmail.com>
2019-02-21 17:56:25 -0800

    Attempt at swapping %3 with 2

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator1.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator1.py
@@ -3,16 +3,18 @@ import sys
 
 def optimal_sequence(n):
     sequence = []
+    sequence2 = []
+    n2 = n
     while n >= 1:
         sequence.append(n)
-        mod3 = n % 3
-        floor3 = n // 3
-        mod2 = n % 2
-        floor2 = n // 2
+        sequence2.append(n2)
 
-        if n == 10 and (n - 1) % 3 == 0:
-            n = n - 1
-            sequence.append(n)
+        if n % 3 == 1 and n % 2 == 0 and n < 12:
+            floor3 = (n - 1) // 3
+            floor2 = n // 2
+            if floor3 < floor2:
+                n = n - 1
+                sequence.append(n)
 
         if n % 3 == 0:
             n = n // 3
@@ -21,7 +23,24 @@ def optimal_sequence(n):
         else:
             n = n - 1
 
-    return reversed(sequence)
+        if n2 % 3 == 1 and n2 % 2 == 0 and n2 < 12:
+            floor3 = (n2 - 1) // 3
+            floor2 = n2 // 2
+            if floor3 < floor2:
+                n2 = n2 - 1
+                sequence2.append(n2)
+
+        if n2 % 2 == 0:
+            n2 = n2 // 2
+        elif n2 % 3 == 0:
+            n2 = n2 // 3
+        else:
+            n2 = n2 - 1
+
+    if len(sequence2) < len(sequence):
+        return reversed(sequence2)
+    else:
+        return reversed(sequence)
 
 input = sys.stdin.read()
 n = int(input)
@@ -30,6 +49,7 @@ print(len(sequence) - 1)
 for x in sequence:
     print(x, end=' ')
 
+# https://stackoverflow.com/questions/22427257/finding-least-number-of-moves
 # 1 = 0 1
 # 96234 = 14    1 3 9 10 11 22 66 198 594 1782 5346 16038 16039 32078 96234
 # or      14    1 3 9 10 11 33 99 297 891 2673 8019 16038 16039 48117 96234
