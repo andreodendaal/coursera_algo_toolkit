368e6bdb4077de17627c48d9a94a44a64d3c07d6
andre odendaal <aodendaal.direct@gmail.com>
2019-02-24 11:49:13 -0800

    two sequences - scratch

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -4,7 +4,18 @@ import sys
 
 def lcs2(a, b):
     #write your code here
-    return min(len(a), len(b))
+    a_lst = list(a)
+    b_lst = list(b)
+    for i in a_lst:
+        if i not in b_lst:
+            a_lst.remove(i)
+
+    for b in b_lst:
+        if b not in a_lst:
+            b_lst.remove(b)
+
+    return min(len(a_lst), len(b_lst))
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,3 +31,7 @@ if __name__ == '__main__':
     b = data[:m]
 
     print(lcs2(a, b))
+
+# 3 2 7 5  2 2 5 = 2
+# 1 7  4 1 2 3 4 = 0
+# 4 2 7 8 3  4 5 2 8 7 = 2
