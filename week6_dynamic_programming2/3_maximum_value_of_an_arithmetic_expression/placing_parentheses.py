2754c2a21de913c4449d8b41175e6573f19ea8da
andreodendaal <aodendaal.direct@gmail.com>
2019-04-01 16:54:53 -0700

    Build min max base function

--- week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -13,16 +13,28 @@ def evalt(a, b, op):
 
 def get_maximum_value(dataset):
     #write your code here
+    global max_grid
+    global min_grid
+
     lst_dataset = list(dataset)
-    integers = re.sub("\D", "", dataset)
-    max_grid = [[0] * len(integers) for _ in range(len(integers))]
+    lst_integers = re.sub("\D", "", dataset)
+    n = len(lst_integers)
+    max_grid = [[0] * len(lst_integers) for _ in range(n)]
 
-    for ctr, val in enumerate(integers):
+    for ctr, val in enumerate(lst_integers):
         max_grid[ctr][ctr] = val
-
     min_grid = max_grid[:]
 
+    for s in range(n - 1):
+        for i in range(n - s):
+            j = i + s
+            MinandMax(i, j)
+
     return 0
+
+
+def MinandMax(i, j):
+    print(max_grid)
 
 
 if __name__ == "__main__":
