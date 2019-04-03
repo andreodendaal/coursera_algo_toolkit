4524bae9fe48309126abb661407bf7df9e2424c4
andre odendaal <aodendaal.direct@gmail.com>
2019-04-02 23:22:15 -0700

    Arithmetic expression - pass

--- week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,6 @@
 # Uses python3
+
+#Pass
 import re
 
 #use eval
@@ -20,23 +22,63 @@ def get_maximum_value(dataset):
     lst_dataset = list(dataset)
     str_integers = (re.sub("\D", "", dataset))
     str_operators = ''.join(i for i in dataset if not i.isdigit())
-    #lst_integers = list(int(str_integers))
-    n = len(str_integers)
-    max_grid = [[0] * len(str_integers) for _ in range(n)]
+
+    leng = len(str_integers)
+    max_grid = [[0] * len(str_integers) for _ in range(leng)]
+    min_grid = [[0] * len(str_integers) for _ in range(leng)]
+
+    # Build initial 2D lists building the main diagonal using initial values
+    #Max grid
+    for ctr, val in enumerate(str_integers):
+        max_grid[ctr][ctr] = int(val)
 
     for ctr, val in enumerate(str_integers):
-        max_grid[ctr][ctr] = val
+        min_grid[ctr][ctr] = int(val)
 
-    min_grid = max_grid[:]
+    for l, val in enumerate(str_integers, start=1):
+    #for l, val in enumerate(str_integers):
 
-    for s, val in enumerate(str_integers, start=1):
-        param_range = n - s
-        for (i) in range(1, n - s):
-            j = i + s
-            print(i)
-            #MinandMax(i, j)
+        for i in range(leng - l):
+        #for i in range(leng - 1):
+        #i = 0
+        #while i < (leng - l):
+            min_tmp = []
+            max_tmp = []
 
-    return 0
+            #j = i + (l + 1)
+            j = i + l
+            k = i
+
+            while k < j:
+                if str_operators[k] == '+':
+                    min_tmp.append(min_grid[i][k] + min_grid[k + 1][j])
+                    max_tmp.append(max_grid[i][k] + max_grid[k + 1][j])
+                    min_tmp.append(min_grid[i][k] + max_grid[k + 1][j])
+                    max_tmp.append(max_grid[i][k] + min_grid[k + 1][j])
+                elif str_operators[k] == '-':
+                    min_tmp.append(min_grid[i][k] - min_grid[k + 1][j])
+                    max_tmp.append(max_grid[i][k] - max_grid[k + 1][j])
+                    min_tmp.append(min_grid[i][k] - max_grid[k + 1][j])
+                    max_tmp.append(max_grid[i][k] - min_grid[k + 1][j])
+                elif str_operators[k] == '*':
+                    min_tmp.append(min_grid[i][k] * min_grid[k + 1][j])
+                    max_tmp.append(max_grid[i][k] * max_grid[k + 1][j])
+                    min_tmp.append(min_grid[i][k] * max_grid[k + 1][j])
+                    max_tmp.append(max_grid[i][k] * min_grid[k + 1][j])
+
+                k += 1
+
+
+            max_amt = max(max_tmp)
+            old_maxval = max_grid[i][j]
+            max_grid[i][j] = max_amt
+            min_amt = min(min_tmp)
+            min_grid[i][j] = min_amt
+
+            #i += 1
+
+    final = max_grid[0][-1]
+    return final
 
 
 def MinandMax(i, j):
@@ -52,5 +94,5 @@ if __name__ == "__main__":
     print(get_maximum_value(input()))
 
 # 5-8+7*4-8+9 = 200
+# 1+2*3+4*5 = 105
 
-# https://www.geeksforgeeks.org/minimum-maximum-values-expression/
